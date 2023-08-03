688a4c36e6477cd83c53d23ff4aa82d428e5485f
Phonekham <30440409+Phonekham@users.noreply.github.com>
2023-08-04 01:01:22 +0700

    firebase setup

--- flutter_instagram/lib/main.dart
+++ flutter_instagram/lib/main.dart
@@ -1,6 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_instagram/responsive/mobile_screen_layout.dart';
+import 'package:flutter_instagram/responsive/responsive_layout.dart';
+import 'package:flutter_instagram/responsive/web_screen_layout.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: 'AIzaSyDOEjktIYchfIVM4Z_OKJExYVgHuGWR13c',
+            appId: '1:954122071176:web:24e6398fde3a04d234833d',
+            messagingSenderId: '954122071176',
+            projectId: 'flutter-firebase-course-20c1a',
+            storageBucket: 'flutter-firebase-course-20c1a.appspot.com'));
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
@@ -10,14 +27,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Instagram',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home:  Scaffold(body: Container(child: Text('data')),),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Instagram',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const ResponsiveLayout(
+          mobileScreenLayout: MobileScreenLayout(),
+          webScreenLayout: WebScreenLayout(),
+        ));
   }
 }
-
-
---
+++ flutter_instagram/lib/responsive/web_screen_layout.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class WebScreenLayout extends StatefulWidget {
+  const WebScreenLayout({super.key});
+
+  @override
+  State<WebScreenLayout> createState() => _WebScreenLayoutState();
+}
+
+class _WebScreenLayoutState extends State<WebScreenLayout> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text('web'));
+  }
+}
